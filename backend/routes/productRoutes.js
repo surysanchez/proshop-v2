b8147b0216750fd94158a4f738ccf6de3f57b031
sury <surysanchezsales@gmail.com>
2023-09-24 11:23:01 -0400

    error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,8 +12,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
         return res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found'); 
     }
-    res.status(404).json({ message: 'Product not found'});
+    
 }));
 
 export default router;
