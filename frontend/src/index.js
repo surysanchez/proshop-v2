30f369b2f9529d6a39278faa53ff90b64eb4f6e7
sury <surysanchezsales@gmail.com>
2023-09-30 23:01:29 -0400

    redux store & api slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -20,7 +22,6 @@ const router = createBrowserRouter(
     < Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
-
     </Route>
   )
 )
@@ -28,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
